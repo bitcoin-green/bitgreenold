1bd3dae39e9ff518de2fbdc0792476f1e82d6a52
David Tai <david@verus.io>
2018-06-27 16:04:49 -0500

    boost-1.66-fix

--- src/bitcoingreen-cli.cpp
+++ src/bitcoingreen-cli.cpp
@@ -105,7 +105,7 @@ UniValue CallRPC(const string& strMethod, const UniValue& params)
     // Connect to localhost
     bool fUseSSL = GetBoolArg("-rpcssl", false);
     asio::io_service io_service;
-    ssl::context context(io_service, ssl::context::sslv23);
+    ssl::context context(ssl::context::sslv23);
     context.set_options(ssl::context::no_sslv2 | ssl::context::no_sslv3);
     asio::ssl::stream<asio::ip::tcp::socket> sslStream(io_service, context);
     SSLIOStreamDevice<asio::ip::tcp> d(sslStream, fUseSSL);
