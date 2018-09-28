23fc9508c4982bd4ed36e7c1a55a3e4f94fdec09
Markus GH <markus.ghambari@gmail.com>
2018-09-28 23:29:05 +1000

    Update masternode-budget.cpp

--- src/masternode-budget.cpp
+++ src/masternode-budget.cpp
@@ -811,7 +811,7 @@ std::vector<CBudgetProposal*> CBudgetManager::GetBudget()
         if (pbudgetProposal->fValid && pbudgetProposal->nBlockStart <= nBlockStart &&
             pbudgetProposal->nBlockEnd >= nBlockEnd &&
             // check the highest budget proposals (+/- 10% to assist in consensus)
-            pbudgetProposal->mapVotes->size() >= nHighestCount - nCountThreshold;
+            pbudgetProposal->mapVotes->size() >= nHighestCount - nCountThreshold &&
             pbudgetProposal->GetYeas() - pbudgetProposal->GetNays() > mnodeman.CountEnabled(ActiveProtocol()) / 10 &&
             pbudgetProposal->IsEstablished()) {
 
