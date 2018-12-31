8ce80f8ea89bda4438441374fcf38f55b987eb28
konez2k <konez2k@gmail.com>
2018-12-31 17:29:50 +0100

    [init] Ignore SIGPIPE
    Ignore SIGPIPE on all non-win32 OSes, otherwise an unexpectedly disconnecting RPC client will terminate the application.

--- src/init.cpp
+++ src/init.cpp
@@ -755,10 +755,8 @@ bool AppInit2()
     sa_hup.sa_flags = 0;
     sigaction(SIGHUP, &sa_hup, NULL);
 
-#if defined(__SVR4) && defined(__sun)
-    // ignore SIGPIPE on Solaris
+    // Ignore SIGPIPE, otherwise it will bring the daemon down if the client closes unexpectedly
     signal(SIGPIPE, SIG_IGN);
-#endif
 #endif
 
     // ********************************************************* Step 2: parameter interactions
