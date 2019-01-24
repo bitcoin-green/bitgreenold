f988c33768d6a629f149e44502fa0fc2a38c6923
konez2k <konez2k@gmail.com>
2019-01-24 23:07:53 +0100

    removed trailing spaces

--- src/main.cpp
+++ src/main.cpp
@@ -2189,7 +2189,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
             it++;
         }
     }
-    
+
 
     // add this block to the view's block chain
     view.SetBestBlock(pindex->GetBlockHash());
